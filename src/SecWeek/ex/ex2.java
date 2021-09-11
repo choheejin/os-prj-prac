178a2a9d1bf71b4ddc962b67e452b634feb35070
choheejin <hjcho9510@gmail.com>
2021-09-12 00:08:50 +0900

    add ex2

---
+++ src/SecWeek/ex/ex2.java
@@ -0,0 +1,12 @@
+package SecWeek.ex;
+
+public class ex2 {
+    public static void main(String[] args){
+        final double PI = 3.14; // 상수 선언
+
+        double radius = 10.0;
+        double circleArea = radius * radius * PI;
+
+        System.out.println("원의 면적 = " + circleArea);
+    }
+}
