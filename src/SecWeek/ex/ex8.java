a7809332b832b5ae55fd922a6368e7b62c90f6c4
choheejin <hjcho9510@gmail.com>
2021-09-12 23:37:22 +0900

    add ex8

---
+++ src/SecWeek/ex/ex8.java
@@ -0,0 +1,12 @@
+package SecWeek.ex;
+
+public class ex8 {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+
+        // condition ? opr2 : opr3
+        // condition(T) => opr2
+        // condition(F) => opr3
+        System.out.println("두 수의 차는 " + ((a>b)?(a-b):(b-a))); // 3>5 => false => (5-3)
+    }
+}
