f5a0485ed6cc4c71520151524f209e10958e27c0
choheejin <hjcho9510@gmail.com>
2021-09-13 00:06:48 +0900

    add ex10

---
+++ src/SecWeek/ex/ex10.java
@@ -0,0 +1,15 @@
+package SecWeek.ex;
+
+import java.util.Scanner;
+
+public class ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("점수를 입력하시오: ");
+        int score = scanner.nextInt(); // 다음 토큰을 int 타입으로 리턴
+
+        if(score >= 80)
+            System.out.println("축하합니다! 합격입니다.");
+    }
+}
