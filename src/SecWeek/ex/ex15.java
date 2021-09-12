4e67a299607238338d320a7864597f04824eb994
choheejin <hjcho9510@gmail.com>
2021-09-13 00:35:58 +0900

    add ex15

---
+++ src/SecWeek/ex/ex15.java
@@ -0,0 +1,29 @@
+package SecWeek.ex;
+
+import java.util.Scanner;
+
+public class ex15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("무슨 커피를 드릴까요?  ");
+        String order = scanner.next();
+
+        int price =0;
+        switch (order){
+            case "에스프레소":
+            case "카푸치노":
+            case "카페라떼":
+                price = 3500;
+                break;
+            case "아메리카노":
+                price = 2000;
+                break;
+            default:
+                System.out.println("메뉴에 존재하지 않는 상품입니다.");
+        }
+
+        if(price != 0)
+            System.out.print(order+"는 " + price + "원입니다.");
+    }
+}
