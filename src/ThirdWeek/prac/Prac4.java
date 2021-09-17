3d7a9f27b7dd75f58fba799e72638e7ab6d511ab
choheejin <hjcho9510@gmail.com>
2021-09-17 22:03:18 +0900

    add Prac4

--- src/ThirdWeek/prac/Prac4.java
+++ src/ThirdWeek/prac/Prac4.java
@@ -1,2 +1,20 @@
-package ThirdWeek.prac;public class Prac4 {
+package ThirdWeek.prac;
+
+import java.util.Scanner;
+
+public class Prac4 {
+    public static void main(String[] args) {
+        System.out.print("소무자 알파벳 하나를 입력하시오>>");
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.next();
+        char c = s.charAt(0);
+
+        for(int i = 0; i <= (c-'a'); i++){
+            for(char a = 'a'; a <= (c - i); a++)
+                System.out.print(a);
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
