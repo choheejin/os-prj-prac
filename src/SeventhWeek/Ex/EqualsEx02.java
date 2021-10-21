f1575fb623556ee7e64bab5064fbc4bae8ba322a
choheejin <hjcho9510@gmail.com>
2021-10-21 17:30:02 +0900

    add Ex04

--- src/SeventhWeek/Ex/EqualsEx02.java
+++ src/SeventhWeek/Ex/EqualsEx02.java
@@ -1,2 +1,30 @@
-package SeventhWeek.Ex;public class EqualsEx02 {
+package SeventhWeek.Ex;
+
+class Rect{
+    int width, height;
+    public Rect(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+    public boolean equals(Object obj){
+        Rect p = (Rect)obj;
+        if(width*height == p.width*p.height)
+            return true;
+        else
+            return false;
+    }
+}
+
+public class EqualsEx02 {
+    public static void main(String[] args) {
+        Rect a = new Rect(2,3);
+        Rect b = new Rect(3,2);
+        Rect c = new Rect(3,4);
+        if(a.equals(b))
+            System.out.println("a is equals to b");
+        if(a.equals(c))
+            System.out.println("a is equals to c");
+        if(b.equals(c))
+            System.out.println("b is equals to c");
+    }
 }
