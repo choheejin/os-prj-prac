bbf20cf8c8f834008a718cd8b34d03d146513b65
choheejin <hjcho9510@gmail.com>
2021-11-18 02:29:51 +0900

    add Ex 10-4

---
+++ src/EleventhWeek/ex/MouseListenerEx.java
@@ -0,0 +1,49 @@
+package EleventhWeek.ex;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseListenerEx extends JFrame {
+    private JLabel la = new JLabel("Hello");
+
+    public MouseListenerEx(){
+        setTitle("Mouse 이벤트 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.addMouseListener(new MyMouseListener());
+
+        c.setLayout(null);
+        la.setSize(50, 20);
+        la.setLocation(30, 30);
+        c.add(la);
+
+        setSize(250, 250);
+        setVisible(true);
+    }
+
+    class MyMouseListener implements MouseListener {
+        public void mousePressed(MouseEvent e){
+            int x = e.getX();
+            int y = e.getY();
+            la.setLocation(x,y);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) { }
+
+        @Override
+        public void mouseEntered(MouseEvent e) { }
+
+        @Override
+        public void mouseExited(MouseEvent e) { }
+
+        @Override
+        public void mouseReleased(MouseEvent e) { }
+    }
+
+    public static void main(String[] args) {
+        new MouseListenerEx();
+    }
+}
